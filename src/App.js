819c1e5ef206c83b2d05abab15669265c2c8a5fa
seungyunkim <seungyunkim@k1-admui-MacBookPro.local>
2020-11-17 19:29:14 -0800

    start react with material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import One from './components/one';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/one" component={One} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/one.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class One extends Component{
+    render(){
+        return(
+            <div className='one'>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default One;
